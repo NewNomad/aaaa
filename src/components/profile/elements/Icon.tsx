3860f895a4e658b2a380e0d947fba77b67f53feb
hiroto2002 <hiroto2002h14@icloud.com>
2023-05-31 19:33:47 +0900

    profile

---
+++ src/components/profile/elements/Icon.tsx
@@ -0,0 +1,23 @@
+import React, { CSSProperties } from "react";
+import Image from "next/image";
+
+type Props = {
+  width: number;
+  height: number;
+};
+export const Icon = ({ width, height }: Props) => {
+  return (
+    <Image
+      src="/profile/other.svg"
+      alt="icon"
+      width={width}
+      height={height}
+      style={styles}
+    />
+  );
+};
+
+const styles: CSSProperties = {
+  borderRadius: 50,
+  margin:"20px 0 0 0"
+};
