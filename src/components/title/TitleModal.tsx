500cc9178a2f06fcb48f99ac2449cf809cfe539c
coa3 <lovecoal928@gmail.com>
2023-05-31 21:23:13 +0900

    add: ログインメニューモーダル

---
+++ src/components/title/TitleModal.tsx
@@ -0,0 +1,112 @@
+import ModalBg from '../ModalBg'
+
+export default function TitleModal() {
+  return (
+    <div>
+      <ModalBg />
+      <div
+        style={{
+          position: 'fixed',
+          width: '100%',
+          height: '50%',
+          bottom: 0,
+          background: 'white',
+          borderRadius: '50px 50px 0 0',
+          display: 'flex',
+          flexDirection: 'column',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <section
+          style={{
+            width: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <input
+            value=""
+            placeholder="メールアドレス"
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              border: "1px solid black",
+              marginBottom: '1rem',
+            }}
+          />
+          <input
+            value=""
+            placeholder="パスワード"
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              border: "1px solid black",
+              marginBottom: '1rem',
+            }}
+          />
+          <a href="/login/logined"
+            style={{
+              width: '222px',
+              backgroundColor: "white",
+              color: "black",
+              border: "1px solid black",
+              marginBottom: '1rem',
+            }}
+          >
+            ログイン
+          </a>
+        </section>
+        <div
+          style={{
+            width: '222px',
+            backgroundColor: "white",
+            color: "black",
+            border: "1px solid black",
+            marginBottom: '1rem',
+          }}
+        >
+          <a href="/register/register">
+            新規登録
+          </a>
+        </div>
+        <section
+          style={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <p style={{marginBottom: '1rem'}}>または</p>
+          <a href="/login/logined"
+            style={{
+              width: '222px',
+              backgroundColor: "white",
+              color: "black",
+              border: "1px solid black",
+              marginBottom: '1rem',
+            }}
+          >
+            ツイッターでログイン
+          </a>
+          <a href="/login/logined"
+            style={{
+              width: '222px',
+              backgroundColor: "white",
+              color: "black",
+              border: "1px solid black",
+              marginBottom: '1rem',
+            }}
+          >
+            グーグルでログイン
+          </a>
+        </section>
+      </div>
+    </div>
+  )
+}
